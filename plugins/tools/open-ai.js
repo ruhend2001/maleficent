156c52647d2f4bdad925ce6e8b075798298d6de1
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-02-03 18:35:09 +0700

    Update open-ai.js

--- plugins/tools/open-ai.js
+++ plugins/tools/open-ai.js
@@ -1,22 +1,18 @@
-import fetch from 'node-fetch'
-import util from 'util';
-
 export default {
    names: ['Tools'],
    tags: ['ai', 'chatgpt', 'openai'],
    command: ['ai', 'chatgpt', 'openai'],
    start: async (m, {
+      conn,
       text,
       prefix,
-      command
+      command,
+      Format
    }) => {
       if (!text) return m.reply(`contoh ${prefix+command} apa kabar?`)
-      await m.adReply('Menunggu Respon', setting.thumbnail, m.chat)
-      let res = await fetch(`https://vihangayt.me/tools/chatgpt?q=${text}`)
-      let open = await res.json()
-      let data = await open.data
-      let ai = await util.format(data)
-      await m.adReply(ai, setting.thumbnail, m.chat)
+      m.adReply(mess.wait, setting.thumbnail, m.chat)
+      let { response } = await Format.Beheaded(text);
+      await m.adReply(response, setting.thumbnail, m.chat)
    },
    limit: 5,
    register: true
