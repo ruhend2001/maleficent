31cd08a98951bbd8a21826bd07ffd95a15b5b08c
Ruli Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-02-06 14:19:12 +0700

    Update susun-kata.js

--- plugins/games/susun-kata.js
+++ plugins/games/susun-kata.js
@@ -11,14 +11,13 @@ export let m = {
       budy,
       User
    }) => {
-      if (susunkata.hasOwnProperty(m.sender.split('@')[0]) && budy && !m.isBaileys) {
+      if (susunkata.hasOwnProperty(m.sender.split('@')[0]) && budy && !budy.includes('.susunkata') && !m.isBaileys) {
          let jawaban = susunkata[m.sender.split('@')[0]]
          if (budy.toLowerCase() === jawaban) {
             await m.adReply(`Benar 🎊 \nkamu mendapatkan:\n+ ${rewards.limit} limit 🎟\n+ ${rewards.uang} uang 💵`, setting.thumbnail, m.chat)
             User.dbPlus(m.sender, rewards);
             delete susunkata[m.sender.split('@')[0]];
-         } else if (budy.includes('.susunkata')) {
-            return {}
+            console.log(susunkata);
          } else {
             await m.adReply(wrong, setting.thumbnail, m.chat);
          }
@@ -40,12 +39,14 @@ export default {
       let result = results[Math.floor(Math.random() * results.length)];
       m.adReply(`Silahkan Jawab Soal Di Atas Ini\n\nDeskripsi : ${result.soal}\n\nTipe ${result.tipe}\nWaktu : 60 Detik\nHadiah 🛍\n${rewards.limit} limit 🎟 dan ${rewards.uang} uang 💵`, "https://lh3.googleusercontent.com/o2SA5NGKG3hTljpBZMnAPG2T7qdhhCk6gvY1tnn1fIm9JvTqnrkIiCL6_FOptI9WpA", m.chat).then(() => {
          susunkata[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
+         console.log(susunkata);
       });
       await Format.sleep(60000);
       if (susunkata.hasOwnProperty(m.sender.split('@')[0])) {
          console.log("Jawaban: " + result.jawaban)
          m.adReply(`📢 Waktu Habis\nJawaban: ${susunkata[m.sender.split('@')[0]]}`, setting.thumbnail, m.chat)
          delete susunkata[m.sender.split('@')[0]]
+         console.log(susunkata);
       }
    }
 };
