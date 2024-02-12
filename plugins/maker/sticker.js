66561c8c4a7433629f6db99f124b783b6d1c27db
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-02-12 11:13:30 +0700

    Update sticker.js

--- plugins/maker/sticker.js
+++ plugins/maker/sticker.js
@@ -14,10 +14,10 @@ export default {
       if (/image|video/.test(mime) || m.mtype === 'imageMessage' || m.mtype === 'videoMessage') {
          if (!quoted) return;
          let buffer = await quoted.download();
-         await m.adReply(mess.wait, setting.thumbnail, m.chat)
+         m.adReply(mess.wait, setting.thumbnail, m.chat)
          conn.sendImageAsSticker(m.chat, buffer, m, {
             packname: pack,
-            author: own
+            author: `${own}\ncreated : \n${waktu.tanggal}\n${waktu.jam} ${waktu.suasana}`
          });
       } else {
          m.reply(`Kirim gambar atau video dengan caption ${prefix + command} atau balas gambar yang sudah dikirim`);
