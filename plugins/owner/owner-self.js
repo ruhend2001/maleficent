e6856cf4ba5ef6e441385f8b44094b73721ae30d
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-04-26 08:53:59 +0700

    Update owner-self.js

--- plugins/owner/owner-self.js
+++ plugins/owner/owner-self.js
@@ -15,12 +15,18 @@ export default {
          change = {
             self: true
          };
-         m.reply(`Mode self berhasil diaktifkan. Hanya aku, owner, dan premium yang dapat mengakses bot ini`);
+         await m.reply(`Mode self berhasil diaktifkan. Hanya aku, owner, dan premium yang dapat mengakses bot ini \nmemulai ulang bot...`);
+         setTimeout(() => {
+            process.send('reset');
+         }, 1000);
       } else if (text.toLowerCase() === "off") {
          change = {
             self: false
          };
-         m.reply(`Mode self berhasil dimatikan. Sekarang semua orang dapat mengakses bot ini`);
+         await m.reply(`Mode self berhasil dimatikan. Sekarang semua orang dapat mengakses bot ini \nmemulai ulang bot...`);
+         setTimeout(() => {
+            process.send('reset');
+         }, 1000);
       } else {
          return m.reply(`Masukkan Parameter yang valid: on/off`);
       }
