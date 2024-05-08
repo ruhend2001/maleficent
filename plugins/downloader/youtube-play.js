a8966e256b96627ab295553454cb16ed48bdd317
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-08 17:54:40 +0700

    Update youtube-play.js

--- plugins/downloader/youtube-play.js
+++ plugins/downloader/youtube-play.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch'
-import { ytmp4, ytsearch } from '../../lib/download.js'
+import { ytmp4, ytmp3v2, ytsearch } from '../../lib/download.js'
 export default {
    names: ['Downloader'],
    tags: ['play', 'song', 'lagu'],
@@ -26,9 +26,11 @@ export default {
       play += ` ⬡ Link: ${url}\n\n`
       play += ` *Loading audio sedang dikirim...*`
       m.adsReply(play, thumbnail, m.chat)
+      let { audio } = await ytmp3v2(url);
       let { video } = await ytmp4(url);
-      let media = await Format.getBuffer(video);
-      let data = await Format.mp3v2(media);
+      let audi0; let progress;
+      try { audi0 = await Format.getBuffer(audio); progress = 'V1'; } catch { audi0 = await Format.getBuffer(video); progress = 'V2'; }
+      let data = await Format.mp3v2(audi0);
       if (data) {
          return conn.sendFile(m.chat, data, {
             mimetype: 'audio/mp4',
@@ -40,7 +42,7 @@ export default {
                   mediaType: 2,
                   mediaUrl: url,
                   title: title,
-                  body: setting.botName,
+                  body: setting.botName + ` ${progress}`,
                   sourceUrl: url,
                   thumbnail: await (await fetch(thumbnail)).buffer()
                }
