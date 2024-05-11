980bb4d6919741b0643f3e6b7caa6b5f0c6ff919
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-11 07:06:56 +0700

    Update user-claimkupon.js

--- plugins/_user/user-claimkupon.js
+++ plugins/_user/user-claimkupon.js
@@ -11,7 +11,7 @@ export default {
          if (typeof remainingTime === "number") {
             return m.reply(`Kamu sudah melakukan claim kupon sebelumnya. Tunggu ${remainingTime} jam lagi sebelum dapat melakukan claim kupon kembali.`);
          } else {
-            var ClaimK = `Claim Kupon berhasil. Kamu dapat ${give} Kupon\nKamu Bisa melakukan claim lagi dalam 72 jam mendatang.`
+            var ClaimK = `Claim Kupon berhasil. Kamu dapat ${give} Kupon\nKamu Bisa melakukan claim lagi dalam 72 jam mendatang.\n\nJika Ingin Tukar Ke Limit Contoh Penggunaannya: \n.tukarkupon 1 \n\nLihat Ada Berapa Kupon Kamu Berapa Banyak Bisa Di Tukar Ke Limit Ketik .me`
             m.adsReply(ClaimK, setting.thumbnail, m.chat)
          }
       } catch (error) {
