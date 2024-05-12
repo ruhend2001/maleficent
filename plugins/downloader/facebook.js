92cf903db9907a7198616c18555f9c4b2be58dba
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-12 18:38:30 +0700

    Update facebook.js

--- plugins/downloader/facebook.js
+++ plugins/downloader/facebook.js
@@ -9,14 +9,14 @@ export default {
       prefix,
       command
    }) => {
-      if (!text) return m.reply(`Masukan link facebook nya! \nContoh: ${prefix+command} https://www.facebook.com/100070546388418/posts/pfbid0279knMA1reA28n52rKTDmDW1wMa88afUHZMGNapEUJQ1bRbQcYMfBaeHz4nhhzNTUl/`);
+      if (!text) return m.reply(`Masukan link facebook nya! \nContoh: ${prefix+command} https://www.facebook.com/reel/3677168492551989?mibextid=rS40aB7S9Ucbxw6v`);
       let res = await igdl(text);
       m.adReply(loading, setting.thumbnail, m.chat);
       let result = res.data;
       let data;
       try {
-         m.reply(`Data Found!`);
-         data = result.find(i => i.resolution === "720p (HD)")        
+         data = result.find(i => i.resolution === "720p (HD)");
+         m.reply(`Data Found!`);       
       } catch {
          m.reply(`HD not found switch to SD`);
          data = result.find(i => i.resolution === "360p (SD)")
