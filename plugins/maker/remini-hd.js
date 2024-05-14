7e098487ca9cc9edad9b55dd4e7c168e5619e7e5
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-14 18:07:07 +0700

    Update remini-hd.js

--- plugins/maker/remini-hd.js
+++ plugins/maker/remini-hd.js
@@ -1,3 +1,4 @@
+/*
 export default {
    names: ['Maker'],
    tags: ['remini', 'hd'],
@@ -25,7 +26,41 @@ export default {
          m.reply(`Balas Atau Kirim image dengan caption ${prefix+command}`)
       }
    },
-   limit: 20,//banyakin limitnya ke enakan usernya nanti kalo limit kecil kan bisa beli premium usernya ke elu :)
+   limit: 25,//banyakin limitnya ke enakan usernya nanti kalo limit kecil kan bisa beli premium usernya ke elu :)
+   premium: false,
+   disable: false
+};
+*/
+
+export default {
+   names: ['Maker'],
+   tags: ['remini', 'hd'],
+   command: ['remini', 'hd', 'hdr'],
+   start: async (m, {
+      conn,
+      text,
+      prefix,
+      command,
+      mime,
+      quoted,
+      Format
+   }) => {
+      if (/image/.test(mime) || m.mtype === 'imageMessage') {
+         if (!quoted) return m.reply(`Balas Atau Kirim image dengan caption ${prefix + command}`)
+         m.react('🕒', m.chat);
+         m.adReply(mess.wait, setting.thumbnail, m.chat);         
+         let content = await conn.downloadAndSaveMediaMessage(quoted);
+         let ran = 'tmp/' + Format.getRandom('.jpg')                           
+         let hasil = await Format.HD2(content, ran)         
+         conn.sendFile(m.chat, hasil, {
+            caption: star + ' Berhasil',
+            quoted: m
+         })      
+      } else {
+         m.reply(`Balas Atau Kirim image dengan caption ${prefix+command}`)
+      }
+   },
+   limit: 10,
    premium: false,
    disable: false
 };
