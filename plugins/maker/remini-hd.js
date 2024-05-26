0cae4ba3e4fb2a00ecb9ea95f8b33ee49ca99ead
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-26 18:30:26 +0700

    Add files via upload

---
+++ plugins/maker/remini-hd.js
@@ -0,0 +1,31 @@
+export default {
+   names: ['Maker'],
+   tags: ['remini', 'hd'],
+   command: ['remini', 'hd', 'hdr'],
+   start: async (m, {
+      conn,
+      text,
+      prefix,
+      command,
+      mime,
+      quoted,
+      Format
+   }) => {
+      if (/image/.test(mime) || m.mtype === 'imageMessage') {
+         if (!quoted) return m.reply(`Balas Atau Kirim image dengan caption ${prefix + command}`)
+         m.react('🕒', m.chat);
+         let content = await quoted.download();
+         m.adReply(mess.wait, setting.thumbnail, m.chat);
+         let data = await Format.HD(content);         
+         conn.sendFile(m.chat, data, {
+            caption: star + ' Berhasil Di Tingkatkan',
+            quoted: m
+         })
+      } else {
+         m.reply(`Balas Atau Kirim image dengan caption ${prefix+command}`)
+      }
+   },
+   limit: 25,//banyakin limitnya ke enakan usernya nanti kalo limit kecil kan bisa beli premium usernya ke elu :)
+   premium: false,
+   disable: false
+};
