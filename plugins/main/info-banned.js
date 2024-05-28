0812fd5155cd6cb9d206550caee4237984d46878
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-28 17:45:46 +0700

    Add files via upload

---
+++ plugins/main/info-banned.js
@@ -0,0 +1,14 @@
+export default {
+   names: ['Main Menu'],
+   tags: ['listbanned', 'listban'],
+   command: ['listbanned', 'listban'],
+   start: async (m, {
+      User
+   }) => {
+      let { bannedUsers } = await User.getBannedUser();
+      let listBanned = await Promise.all(bannedUsers.map(bannedUser => ` \n${bannedUser}`))
+      let text = `Berikut Adalah List Pengguna Terbanned ${setting.botName}\n`
+      text += `${listBanned.join('\n')}`
+      m.adReply(text, setting.thumbnail, m.chat);
+   }
+};
