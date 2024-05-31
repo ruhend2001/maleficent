aff2703da4906e63587c38d86f83c2a9f7e7b6d4
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-05-31 18:45:20 +0700

    Update upload.js

--- plugins/tools/upload.js
+++ plugins/tools/upload.js
@@ -11,26 +11,15 @@ export default {
       quoted,
       Format
    }) => {
-      if (!mime && !quoted) return
-      if (/webp/.test(mime) || m.mtype === 'stickerMessage') {
-         let media = await conn.downloadAndSaveMediaMessage(quoted)
-         m.reply(mess.wait);
-         let buffer_up = fs.readFileSync(media);
-         let rand2 = 'tmp/' + Format.getRandom('.webp');
-         fs.writeFileSync(`./${rand2}`, buffer_up);
-         let { name, url, size } = await Format.upload(rand2);
-         let sizeNy = Format.bytesToSize(size);
-         let Upload = `UPLOAD SUKSES\n*Url :* ${url}\n`
-         Upload += `${java} *Name :* ${name}\n`
-         Upload += `${java} *Size :* ${sizeNy}\n`
-         Upload += `${java} *Type:* Sticker`
-         m.adReply(Upload, setting.thumbnail, m.chat)
-      } else if (/image/.test(mime) || m.mtype === 'imageMessage' ||
+      if (/image/.test(mime) || m.mtype === 'imageMessage' ||
          /audio/.test(mime) || m.mtype === 'audioMessage' ||
+         /webp/.test(mime) || m.mtype === 'stickerMessage' ||
          /video/.test(mime) || m.mtype === 'videoMessage') {
-         let mediaType;
-         let fileExt;
-         if (/image/.test(mime) || m.mtype === 'imageMessage') {
+         let mediaType; let fileExt;
+         if (/webp/.test(mime) || m.mtype === 'stickerMessage') {
+            mediaType = 'sticker';
+            fileExt = '.webp';
+         } else if (/image/.test(mime) || m.mtype === 'imageMessage') {
             mediaType = 'image';
             fileExt = '.jpeg';
          } else if (/audio/.test(mime) || m.mtype === 'audioMessage') {
@@ -39,22 +28,22 @@ export default {
          } else if (/video/.test(mime) || m.mtype === 'videoMessage') {
             mediaType = 'video';
             fileExt = '.mp4';
-         }
+         } 
          let media = await conn.downloadAndSaveMediaMessage(quoted)
-         m.reply(mess.wait);
+         m.adReply(loading, setting.thumbnail, m.chat);
          let buffer_up = fs.readFileSync(media);
-         let rand2 = 'tmp/' + Format.getRandom(`${fileExt}`);
+         let rand2 = `tmp/` + Format.getRandom(`${fileExt}`);
          fs.writeFileSync(`./${rand2}`, buffer_up);
          let { name, url, size } = await Format.upload(rand2);
          let sizeNy = Format.bytesToSize(size);
-         let Upload = `UPLOAD SUKSES\n`
-         Upload += `${java} *Url :* ${url}\n`
-         Upload += `${java} *Name :* ${name}\n`
-         Upload += `${java} *Size :* ${sizeNy}\n`
-         Upload += `${java} *Type:* ${mediaType.charAt(0).toUpperCase() + mediaType.slice(1)}`
-         m.adReply(Upload, setting.thumbnail, m.chat)
+         let caption = `*Sukses Upload*\n`
+         caption += `${java} *Url :* ${url}\n`
+         caption += `${java} *Name :* ${name}\n`
+         caption += `${java} *Size :* ${sizeNy}\n`
+         caption += `${java} *Type:* ${mediaType.charAt(0).toUpperCase() + mediaType.slice(1)}`
+         m.adReply(caption, setting.thumbnail, m.chat)
       } else {
-         return m.reply('Balas Media Atau Kirim Media Dengan Caption .tourl atau .upload')
+         return m.reply(`Balas Media Atau Kirim Media Dengan Caption ${prefix}upload atau ${prefix}tourl`);
       }
    },
    limit: 15,
