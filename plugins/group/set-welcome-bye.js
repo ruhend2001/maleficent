67b83e98b281acff98681168df74dc672382dfd3
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-04 13:07:42 +0700

    Add files via upload

---
+++ plugins/group/set-welcome-bye.js
@@ -0,0 +1,31 @@
+export default {
+   names: ['Group'],
+   tags: ['setwelcome', 'setbye'],
+   command: ['setwelcome', 'setbye'],
+   start: async (m, {
+      conn,
+      text,
+      prefix,
+      User,
+      command,
+      groupName        
+   }) => {
+      if (/setwelcome/.test(command)) {
+         if (!text) return m.reply(`Masukan Text Welcome nya! \n\nContoh:\n${prefix+command} Hey Selamat Datang Babi %user\nDi Group %subject \nBaca Deskripsi Yah \n\n%subject adalah Nama Group \n%user adalah tag ke nomor member nya`)
+            const change = {
+            welcomeCaption: text
+         }
+         User.switchGroup(m.chat, change);
+         m.reply(`Caption Welcome Berhasil Di Ganti Di Group ${groupName}`);
+      } else if (/setbye/.test(command)) {
+         if (!text) return m.reply(`Masukan Text Bye nya! \n\nContoh:\n${prefix+command} Beban Group %user\nTelah Keluar Dari %subject \nSemoga ... \n\n%subject adalah Nama Group \n%user adalah tag ke nomor member nya`)
+            const change = {
+            byeCaption: text
+         }
+         User.switchGroup(m.chat, change);
+         m.reply(`Caption Bye Berhasil Di Ganti Di Group ${groupName}`);
+      }
+   },
+   admin: true, //owner allowed
+   group: true
+}
