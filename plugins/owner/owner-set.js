cdabf3433acbc9e32332789a6c8007b1b7edf5e6
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-10 00:20:51 +0700

    Update owner-set.js

--- plugins/owner/owner-set.js
+++ plugins/owner/owner-set.js
@@ -1,14 +1,15 @@
+var boot = () => process.send('reset');
 export default {
    names: ['Owner'],
-   tags: ['set', 'setnamebot', 'setbotname', 'setnameowner', 'setnameown', 'setfooter', 'setwm', 'setsosmed', 'setmusic', 'setram', 'ram', 'setlink', 'setlinkgc'],
-   command: ['set', 'setnamebot', 'setbotname', 'setnameowner', 'setnameown', 'setfooter', 'setwm', 'setsosmed', 'setmusic', 'setram', 'ram', 'setlink', 'setlinkgc'],
+   tags: ['set', 'setnamebot', 'setbotname', 'setnameowner', 'setnameown', 'setfooter', 'setwm', 'setsosmed', 'setmusic', 'setram', 'ram', 'setlink', 'setlinkgc', 'setgroupmode', 'setgcmode'],
+   command: ['set', 'setnamebot', 'setbotname', 'setnameowner', 'setnameown', 'setfooter', 'setwm', 'setsosmed', 'setmusic', 'setram', 'ram', 'setlink', 'setlinkgc', 'setgroupmode', 'setgcmode'],
    start: async (m, {
       text,
       prefix,
       command,
       User,
       Format
-   }) => {
+   }) => {      
       if (command == 'set') {
          var caption = `*SETTING OWNER* \n*Perintah tersedia untuk mengatur setting hanya ada berikut ini*\n\n\n`
          caption += `.setnamebot atau .setbotname \nUntuk mengganti nama bot \n\n`
@@ -18,7 +19,8 @@ export default {
          caption += `.setmusic \nUntuk mengganti link music \n\n`
          caption += `.setram atau .ram \nUntuk mengganti nilai ram \n\n` 
          caption += `.setthumb atau setthumbnail atau .sthumb \nUntuk mengganti thumbnail utama bot \n\n`
-         caption += `.setlink atau setlinkgc \nUntuk mengganti setting link group`
+         caption += `.setlink atau setlinkgc \nUntuk mengganti setting link group\n\n`
+         caption += `.setgroupmode atau .setgcmode\nUntuk mengganti akses bot ke mode group atau keduanya private and group`
          return m.reply(caption);
       } else if (/setnamebot|setbotname/.test(command)) {
          if (!text) return m.reply(`Masukan Nama Bot nya! \nContoh\n${prefix+command} Maleficent-bot`);
@@ -27,7 +29,7 @@ export default {
          });
          m.reply(`Sukses mengganti nama bot menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setnameowner|setnameown/.test(command)) {
          if (!text) return m.reply(`Masukan Nama Nya! \nContoh\n${prefix+command} Ruly Henderson`);
          User.changeOwnerName({ 
@@ -35,7 +37,7 @@ export default {
          });
          m.reply(`Sukses Mengganti Nama Owner Bot Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setfooter|setwm/.test(command)) {
          if (!text) return m.reply(`Masukan nama footer atau watermark nya! \nContoh\n${prefix+command} © Ruhend`);
          User.changeFooter({ 
@@ -43,7 +45,7 @@ export default {
          });
          m.reply(`Sukses Mengganti Footer atau Watermark Bot Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setsosmed/.test(command)) {
          if (!text) return m.reply(`Masukan link sosmed nya! \nContoh\n${prefix+command} www.instagram.com/ru_hend_`);
          User.changeSosmed({ 
@@ -51,7 +53,7 @@ export default {
          });
          m.reply(`Sukses Mengganti Link Sosmed Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setmusic/.test(command)) {
          if (!text) return m.reply(`Masukan link musicnya nya! \nContoh\n${prefix+command} https://MyMusic.mp3 \natau upload music menjadi url`);
          User.changeMusic({ 
@@ -59,7 +61,7 @@ export default {
          });
          m.reply(`Sukses Mengganti Link Music Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setram|ram/.test(command)) {
          /**
          nilai ram ini penting untuk mencegah terjadinya overload 
@@ -72,14 +74,31 @@ export default {
          });
          m.reply(`Sukses Mengganti Nilai RAM Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
+         boot();
       } else if (/setlink|setlinkgc/.test(command)) {
          if (!text) return m.reply(`Masukan link group nya! \nContoh\n${prefix+command} ${setting.group.link}\nAtau link lain juga bisa`);
          User.changeGroupLink(text);
          m.reply(`Sukses Mengganti Link Setting Menjadi ${text}\n\nRestarting....`);
          await Format.sleep(2000);
-         process.send('reset');
-      }
+         boot();
+      } else if (/setgroupmode|setgcmode/.test(command)) {         
+         if (!text) return m.reply(`masukan parameternya contoh \n${prefix+command} on atau off`);
+         let active = true
+         let deactive = false
+         if (text.toLowerCase() == 'on') {
+            User.changeGroupMode(active);
+            m.reply(`Sukses Mengubah Ke Group Mode \nPrivate Chat Tidak Bisa Di Akses Kecuali Aku , Owner Dan Premium\n\nRestarting....`);
+            await Format.sleep(2000);
+            boot();
+         } else if (text.toLowerCase() == 'off') {
+            User.changeGroupMode(deactive);
+            m.reply(`Sukses Mematikan Group Mode Sekarang Private Chat Dapat Diakses\n\nRestarting....`);
+            await Format.sleep(2000);
+            boot();
+         } else {
+            return m.reply(`Masukan parameter yang valid on/off \nContoh\n${prefix+command} on\nAtau\n${prefix+command} off`);                
+         } 
+      }      
    },
    owner: true
 };
