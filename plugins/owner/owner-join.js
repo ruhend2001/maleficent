b18445318406e60135e89e469341edfd5a48b445
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-11 09:45:04 +0700

    Update owner-join.js

--- plugins/owner/owner-join.js
+++ plugins/owner/owner-join.js
@@ -1,6 +1,3 @@
-function isUrl(url) {
-   return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
-};
 export default {
    names: ['Owner'],
    tags: ['join'],
@@ -10,12 +7,13 @@ export default {
       text,
       args
    }) => {
-      if (!text) return m.reply('Masukkan Link Groupnya!')
-      if (!isUrl(args[0]) && !args[0].includes('whatsapp.com')) return m.reply('Link Invalid!')
-      m.reply('Menyatukan...')
+      if (!text) return m.reply('Masukkan Link Groupnya!');
+      let isUrl = (url) => url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'));
+      if (!isUrl(args[0]) && !args[0].includes('whatsapp.com')) return m.reply('Link Invalid!');
+      m.reply('Menyatukan...');
       let result = args[0].split('https://chat.whatsapp.com/')[1]
       await conn.groupAcceptInvite(result)
       m.reply('Berhasil Bersatu Ke Group Yang Kamu Berikan')
    },
    owner: true
-};
+}
