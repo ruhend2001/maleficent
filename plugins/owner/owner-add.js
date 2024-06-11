002e798cfd747754a1bd8ac3f67ae6a0d96829d9
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-11 10:08:17 +0700

    Add files via upload

---
+++ plugins/owner/owner-add.js
@@ -0,0 +1,21 @@
+export default {
+   names: ['Owner'],
+   tags: ['addowner', 'delowner'],
+   command: ['addowner', 'addown', 'delowner', 'delown'],
+   start: async (m, {
+      text,
+      prefix,
+      User,
+      command
+   }) => {
+      if (/addowner|addown/.test(command)) {
+         if (!text) return m.reply(`Masukkan Nomornya contoh\n${prefix+command} 62xxxx`);
+         await User.addOwner(text);         
+         m.reply(`Sukses Menambahkan ${text} sebagai owner`);
+      } else if (/delowner|delown/.test(command)) {
+         if (!text) return m.reply(`Masukkan Nomor yang ingin dihapus dari daftar owner.`);
+         await User.removeOwner(m, text);        
+      }
+   },
+   owner: true
+};
