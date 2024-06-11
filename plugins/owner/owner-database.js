cecac846cb714ebc35c1543b923873112d893383
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-11 09:49:24 +0700

    Update owner-database.js

--- plugins/owner/owner-database.js
+++ plugins/owner/owner-database.js
@@ -4,23 +4,13 @@ export default {
    command: ['db', 'database', 'getdb'],
    start: async (m, {
       conn,
-      sleep,
       User,
       Format
    }) => {
       let { data, name, mime } = await User.getDB()
       m.reply(`Tunggu sedang mengambil file database...`)
-      await Format.sleep(3500) //this depending on your user if too many users i think 5000 is okay
-      conn.sendMessage(m.chat, {
-         document: {
-            url: data
-         },
-         caption: 'Berhasil Backup database',
-         mimetype: mime,
-         fileName: name
-      }, {
-         quoted: m
-      })
+      await Format.sleep(3500);
+      conn.sendMessage(m.chat, { document: { url: data }, caption: 'Berhasil Backup database', mimetype: mime, fileName: name }, { quoted: m });
    },
    owner: true
-};
+}
