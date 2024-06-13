cc370ce48899dac273d2b3ea1134150d8d840b74
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-13 19:22:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('🕒 Starting Meleficent . . .')
+console.log('🕒 Starting Meleficent . . .');
 import path, {
    dirname
 } from 'path'
@@ -8,26 +8,28 @@ import {
 import {
    fileURLToPath
 } from 'url'
+import { 
+   __on
+} from 'maleficent-bot/lib/__on.js'; 
 process.on('uncaughtException', console.log);
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const start = () => {
    let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
    let p = spawn(process.argv[0], args, {
-         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-      })
-      .on('message', data => {
-         if (data == 'reset') {
-            console.log('🕒 Restarting Meleficent . . .')
-            p.kill();
-         }
-         if (data == 'uptime') {
-            p.send(process.uptime())
-         }
-      })
-      .on('exit', code => {
-         console.error('Exited with code:', code)
+      stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+   })
+   .on('message', data => {
+      if (data == 'reset') {
+         console.log('🕒 Restarting Meleficent . . .')
          p.kill();
-         start();
-      })
+      }
+      if (data == 'uptime') {
+         p.send(process.uptime())
+      }
+   })
+   .on('exit', code => {
+      console.error('Exited with code:', code)
+      p.kill(); __on(); start();                  
+   })
 };
-start();
+__on(); start();
