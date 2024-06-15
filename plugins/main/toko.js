758f9678a5c1a88f2d2e2a70d9fa7de43b914c0d
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-16 06:11:11 +0700

    Update toko.js

--- plugins/main/toko.js
+++ plugins/main/toko.js
@@ -1,49 +1,38 @@
-/*
-pakai fitur ini kalo kalian punya dagangan kalian kreasikan sendiri pesannya
-ini hanya tulis tampilkan ampe 5 doank nambahin sendiri kalo punya banyak dagangan
-*/
-import fs from "fs";
+/**
+ * send with media use conn.sendFile or conn.sendButton or Whatever instead that was just an example.
+ * kalo misal nya muncil.blank panjang ya berarti g ada text atau .addtoko yang di masukan seperlunya kalian saja
+ * hapus saja atau kurangi jika terlalu kebanyakan caption tokonya kalo kosong menghasilkan blank 
+ */
 export default {
    names: ['Main Menu'],
-   tags: ['toko', 'dagangan'],
-   command: ['toko', 'dagangan'],
+   tags: ['toko', 'shop'],
+   command: ['toko', 'shop'],
    start: async (m, {
-      conn
+      conn,
+      User
    }) => {
-      let toko = await JSON.parse(fs.readFileSync('./database/toko/toko.json'));
-      let dagangan1 = toko[0].toko1;
-      let dagangan2 = toko[0].toko2;
-      let dagangan3 = toko[0].toko3;
-      let dagangan4 = toko[0].toko4;
-      let dagangan5 = toko[0].toko5;
-      let dagangan8 = toko[0].toko8;
-      /**
-      jika ingin ada medianya seperti gambar pake      
-      conn.sendFile(m.chat, 'url foto atau video', {
-         caption: `sesuaikan saja mau naro dagangan mana contoh ${dagangan1}`,
-         quoted: m
-      })
-      **/
-      m.reply(`
-*My Shop ??*
-1.DIAMOND ML
-${dagangan1}
-
-2.DIAMOND FF
-${dagangan2}
-
-3.RUJAK
-${dagangan3}
-
-4.SAYUR
-${dagangan4}
-
-5.Lainnya
-${dagangan5}
-
-8.Toko8
-${dagangan8}
-
-`)
+      let toko = await User.Toko();
+      let shop1 = toko[0].toko1;
+      let shop2 = toko[0].toko2;
+      let shop3 = toko[0].toko3;
+      let shop4 = toko[0].toko4;
+      let shop5 = toko[0].toko5;
+      let shop6 = toko[0].toko6;
+      let shop7 = toko[0].toko7;
+      let shop8 = toko[0].toko8;
+      let shop9 = toko[0].toko9;
+      let shop10 = toko[0].toko10;      
+      let caption = `List Toko atau Dagangan\n${setting.botName}\n\n`
+      caption += `${shop1}\n\n`
+      caption += `${shop2}\n\n`
+      caption += `${shop3}\n\n`
+      caption += `${shop4}\n\n`
+      caption += `${shop5}\n\n`
+      caption += `${shop6}\n\n`
+      caption += `${shop7}\n\n`
+      caption += `${shop8}\n\n`
+      caption += `${shop9}\n\n`
+      caption += `${shop10}\n\n`
+      m.adsReply(caption, setting.thumbnail, m.chat);     
    }
 };
