0de514b8731f0789e9e5ef953727e370fb8eff02
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-16 06:12:37 +0700

    Update addlist-toko.js

--- plugins/owner/addlist-toko.js
+++ plugins/owner/addlist-toko.js
@@ -8,7 +8,7 @@ export default {
       User,
       command
    }) => {
-      if (!text) return m.reply(`Masukkan dagangan ke toko.\ncontoh ${prefix + command} nama toko item\n\nContoh pemakaian:\n${prefix + command} toko1(spasi)item1 atau textnya  \n${prefix + command} toko2 itemnya atau listnya \n\nDaftar List store atau toko aku adalah\n1. diamon ml\n2. diamon ff\ndan seterusnya contoh \n.addtoko toko1 text \n sepanjang panjangnya juga bisa`);
+      if (!text) return m.reply(`Masukkan dagangan ke toko.\ncontoh ${prefix + command} nama toko item\n\nContoh pemakaian:\n${prefix + command} toko1(spasi)Judul enter dan text Nya \n${prefix + command} toko2 Judul dan listnya\ntextnya \ndan seterusnya\n contoh \n.addtoko toko1 1.List Panel enter\n- Panel Ram 1GB 1000 \n- Panel Ram 2GB 2000\ndan seterusnya...\n\nSepanjang panjangnya juga bisa\nUntuk Mengatur menampilkan caption text toko nya silahkan edit di plugins/main/toko.js (bawaan sc)`);
       let [toko, ...items] = text.split(" ");
       if (!items.length) return m.reply(`Masukkan item yang ingin ditambahkan ke ${toko}.`);
       let item = items.join(" ");
