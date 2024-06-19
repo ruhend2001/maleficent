394e8f644d282f1bb06953bcc4f3ed0a91b70ff2
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-19 19:52:53 +0700

    Update apk-dl.js

--- plugins/downloader/apk-dl.js
+++ plugins/downloader/apk-dl.js
@@ -9,35 +9,27 @@ export default {
       prefix,
       command
    }) => {
-      if (!text) return m.reply(`Masukan Apk yang ingin di cari contoh ${prefix+command} facebook lite`);
+      if (!text) return m.reply(`Masukan apk yang ingin di cari contoh ${prefix+command} facebook lite`);
       if (m.isBaileys) return
-      let res = await download(text)
-      await m.adReply(mess.wait, setting.thumbnail, m.chat)      
-      let icon = await res.icon
-      let paket = await res.package
-      let size = await res.size
-      let nama = await res.name
-      let up = await res.lastup
-      let file = await res.dllink
-      let Apk = `*𝐀𝐏𝐊 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃*\n`
-      Apk += ` ${java} ${paket}\n`
-      Apk += ` ${java} ${size}\n\n`
-      Apk += ` ${java} Nama : ${nama}\n`
-      Apk += ` ${java} Update : ${up}\n`
-      Apk += ` ${java} Nama Paket : ${paket}\n`
-      Apk += ` ${java} Size : ${size}\n`
-      Apk += `*Sending File...*`
-      m.adsReply(Apk, icon, m.chat)
-      conn.sendMessage(m.chat, {
-         document: {
-            url: file
-         },
-         fileName: nama + '.apk',
-         mimetype: 'application/vnd.android.package-archive'
-      }, {
-         quoted: m
-      })
+      let res = await download(text);
+      m.adReply(mess.wait, setting.thumbnail, m.chat);
+      let icon = res.icon
+      let paket = res.package
+      let size = res.size
+      let nama = res.name
+      let up = res.lastup
+      let file = res.dllink
+      let caption = ` 𝐀𝐏𝐊 𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃\n`
+      caption += ` ${java} ${paket}\n`
+      caption += ` ${java} ${size}\n\n`
+      caption += ` ${java} Nama : ${nama}\n`
+      caption += ` ${java} Update : ${up}\n`
+      caption += ` ${java} Nama Paket : ${paket}\n`
+      caption += ` ${java} Size : ${size}\n\n`
+      caption += ` *Sending File...*`
+      m.adsReply(caption, icon, m.chat);
+      conn.sendMessage(m.chat, { document: { url: file }, fileName: nama + '.apk', mimetype: 'application/vnd.android.package-archive' }, { quoted: m });
    },
-   limit: 10,
+   limit: 5,
    premium: false
 };
