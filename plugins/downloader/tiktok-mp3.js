618d809ad675f0a935f42936f0b4168948f8ee03
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-19 16:11:46 +0700

    Update tiktok-mp3.js

--- plugins/downloader/tiktok-mp3.js
+++ plugins/downloader/tiktok-mp3.js
@@ -11,8 +11,8 @@ export default {
    }) => {
       if (!text) return m.reply(`Masukan link tiktok nya! \nContoh: ${prefix + command} https://vt.tiktok.com/ZSNYfYdLj`);
       m.adReply(mess.wait, setting.thumbnail, m.chat)
-      let { audio } = await ttdl(text);
-      conn.sendFile(m.chat, audio, {
+      let { music } = await ttdl(text);
+      conn.sendFile(m.chat, music, {
          mimetype: 'audio/mp4',
          ptt: true,
          quoted: m
