e9ca83c922bf588ed86bf91e1239539048afb204
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-19 20:40:37 +0700

    Add files via upload

---
+++ plugins/owner/owner-get-plugins.js
@@ -0,0 +1,20 @@
+import { exec } from 'child_process'
+import util from 'util'
+export default {
+   names: ['Owner'],
+   tags: ['getplugins'],
+   command: ['gp', 'getplugins'],
+   start: (m, {
+      text,
+      prefix,
+      command
+   }) => {
+      if (!text) return m.reply(`contoh ${prefix+command} plugins/main/help.js atau file plugins yang ingin kamu lihat`);
+      let path = `${text}`;
+      exec(`cat ${path}`, (x, y) => {
+         if (x) return m.reply(`${path}\ntidak ada`);
+         if (y) return m.reply(util.format(y));
+      })      
+   },
+   owner: true
+};
