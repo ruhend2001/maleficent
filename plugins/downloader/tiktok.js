f1101262549955626104f177f37d0264b307e1d7
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-22 15:02:51 +0700

    Update tiktok.js

--- plugins/downloader/tiktok.js
+++ plugins/downloader/tiktok.js
@@ -12,17 +12,19 @@ export default {
       if (!text) return m.reply(`Masukan Tiktok contoh\n${prefix+command}` + ' https://vt.tiktok.com/ZSYfBvx5d/')
       let { desc, avatar, name, like, comment, share, video } = await ttdl(text);
       m.adReply(loading, setting.thumbnail, m.chat);      
-      let caption = `𝐓𝐈𝐊𝐓𝐎𝐊\n`
+      let caption = `${star} 𝐓𝐈𝐊𝐓𝐎𝐊 ${star} \n`
+      caption += `${setting.botName}\n`
       caption += `⭔ Name: ${name}\n`
       caption += `⭔ Description : ${desc}\n`
       caption += `⭔ Like: ${like}\n`
       caption += `⭔ Comment: ${comment}\n`
-      caption += `⭔ Share: ${share}\n\n`
-      caption += `${star} ${setting.botName}`
-      conn.sendFile(m.chat, video, {
-         caption: caption,
-         quoted: m
-      });                    
+      caption += `⭔ Share: ${share}`
+      m.adReply(name, avatar, m.chat).then(() => {
+         conn.sendFile(m.chat, video, {
+            caption: caption,
+            quoted: m
+         });
+      });
    },
    limit: 2
 };
