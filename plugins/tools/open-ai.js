369d185ef49f8dd1ba697d0cb31962601f14359a
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-22 21:48:31 +0700

    Update open-ai.js

--- plugins/tools/open-ai.js
+++ plugins/tools/open-ai.js
@@ -9,10 +9,17 @@ export default {
       command,
       Format
    }) => {
-      if (!text) return m.reply(`contoh ${prefix+command} apa kabar?`)
-      m.adReply(mess.wait, setting.thumbnail, m.chat)
-      let { response } = await Format.Beheaded(text);
-      await m.adReply(response, setting.thumbnail, m.chat)
+      if (!text) return m.reply(`contoh ${prefix+command} apa kabar?`);
+      try {
+         m.adReply(mess.wait, setting.thumbnail, m.chat)
+         let { response } = await Format.Beheaded(text);
+         await m.adReply(response, setting.thumbnail, m.chat);
+      } catch {
+         let { GPT } = await import('free-gpt-turbo');
+         m.reply(`${loading} v2`);
+         let data = await GPT(text);
+         return m.adReply(data, setting.thumbnail, m.chat);  
+      }
    },
    limit: 5,
    register: true
