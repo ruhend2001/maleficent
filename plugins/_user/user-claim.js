4497b633cf461d2c115669d59d21500962011a0d
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-23 13:39:01 +0700

    Update user-claim.js

--- plugins/_user/user-claim.js
+++ plugins/_user/user-claim.js
@@ -3,21 +3,24 @@ export default {
    tags: ['claim'],
    command: ['claim', 'daily'],
    start: async (m, {
+      conn,
       User
    }) => {
       try {
-         let give = 25;
+         let give = 25;         
          let remainingTime = await User.confirmclaim(m.sender, give);
          if (typeof remainingTime === "number" && !m.fromMe) {
             return m.reply(`Kamu sudah melakukan claim sebelumnya. Tunggu ${remainingTime} jam lagi sebelum dapat melakukan claim kembali.`);
          } else {
             if (!m.fromMe) {
-               var Claim = `Claim berhasil. Kamu dapat ${give} Limit\nKamu Bisa melakukan claim lagi dalam 24 jam mendatang.`
-               m.adReply(Claim, setting.thumbnail, m.chat)
+               let b1 = ['Tukar Kupon', '.tukarkupon'];
+               let Claim = `Claim berhasil. Kamu dapat ${give} Limit\nKamu Bisa melakukan claim lagi dalam 24 jam mendatang.`
+               await m.adReply(Claim, setting.thumbnail, m.chat);
+               conn.sendButton(m.chat, setting.thumbnail, '', b1, m);
             }
          }
       } catch (error) {
-         m.reply("Data pengguna tidak ditemukan.");
+         return m.reply("Data pengguna tidak ditemukan.");
       }
    },
    register: true
