c94ce85543a273a7f3643d5fd76e77bd9b4dd832
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-28 04:09:28 +0700

    Update user-unreg.js

--- plugins/_user/user-unreg.js
+++ plugins/_user/user-unreg.js
@@ -9,12 +9,12 @@ export default {
       User,
       Format
    }) => {
-      if (!User.checkRegisteredUser(m.sender)) return m.reply(`Kamu Belum Terdaftar`);
+      if (!User.checkRegisteredUser(m.sender)) return m.reply(`Kamu Belum Terdaftar Ketik .daftar`);
       if (!text) return m.reply(`Masukan nomor sn nya untuk melihat sn silahkan ketik .sn atau .ceksn\ncontoh ${prefix+command} sn `);
       let serial = User.getProfileData(m.sender).seri;
       if (text == serial) {
-         await User.unregistering({ id: m.sender });
-         await Format.sleep(2500);
+         User.unregistering({ id: m.sender });
+         await Format.sleep(1000);
          m.adReply(`Kamu Berhasil Keluar Dari Data Base Semua Data Kamu Kereset Ulang Ketik .me`, setting.thumbnail, m.chat);
       } else {
          return m.reply(`serial number kamu salah masukan dengan benar ketik .sn`);
