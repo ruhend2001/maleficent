07785a2f86ecbded3d64a48d5cc60d2c798ed4ab
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-28 21:46:22 +0700

    Update user-profile.js

--- plugins/_user/user-profile.js
+++ plugins/_user/user-profile.js
@@ -4,6 +4,9 @@ export default {
    command: ['profile', 'my', 'me', 'balance', 'profil'],
    start: async (m, {
       conn,
+      text,
+      prefix,
+      command,
       User
    }) => {
       let isPremium = User.checkPremiumUser(m.sender);
@@ -13,9 +16,38 @@ export default {
       let reg = isRegister ? 'Sudah Daftar' : 'Belum Daftar';
       let limitUser = User.checkLimitUser(m.sender);
       let userData = User.getProfileData(m.sender);
-      if (userData) {
+      let tag = text.match(/@/g);
+      let numTag = text.replace('@', '').replace(prefix, '').replace(command, '').trim();
+      let mention = `${numTag}@s.whatsapp.net`
+      let pictureTag = await conn.profilePictureUrl(mention, 'image').catch(_ => setting.thumbnail);
+      let userTag = await User.getProfileData(mention);
+      let isRegisterTag = User.checkRegisteredUser(mention);
+      let limitUserTag = User.checkLimitUser(mention);
+      let regTag = isRegisterTag ? 'Sudah Daftar' : 'Belum Daftar';
+      let isPremiumTag = User.checkPremiumUser(mention);
+      let premTag = isPremiumTag ? 'Aktif' : 'Tidak';
+      if (tag) {
+         try {
+            let _regtime = `${userTag.registerTime === "" ? "" : '\n ‎ ‎ ‎ ‎ ‎ ‎ ' + userTag.registerTime}`;
+            let Other = `👤 *User Profile* @${numTag}\n`
+            Other += `🏷 Terdaftar: ${regTag}\n`
+            Other += `🗓 Waktu Daftar:${_regtime}\n`
+            Other += `📌 Premium: ${premTag}\n`
+            Other += `📍 Nama: ${userTag.nama}\n`
+            Other += `💋 Umur: ${userTag.umur}\n`
+            Other += `📎 Seri: ${userTag.seri}\n`
+            Other += `🔖 Limit: ${limitUserTag}\n`
+            Other += `💰 Uang: ${userTag.uang}\n`
+            Other += `🛍 Kupon: ${userTag.kupon}\n`
+            m.adsReply(Other, pictureTag, m.chat, {
+               mentions: mention
+            });
+         } catch {
+            return m.reply('Profile Not Active');
+         }
+      } else if (!tag) {
          let Regtime = `${userData.registerTime === "" ? "" : '\n ‎ ‎ ‎ ‎ ‎ ‎ ' + userData.registerTime}`;
-         let Profile = `👤 *User Profile* @${m.sender.split('@')[0]}\n\n`
+         let Profile = `👤 *User Profile* @${m.sender.split('@')[0]}\n`
          Profile += `🏷 Terdaftar: ${reg}\n`
          Profile += `🗓 Waktu Daftar:${Regtime}\n`
          Profile += `📌 Premium: ${prem}\n`
@@ -25,9 +57,7 @@ export default {
          Profile += `🔖 Limit: ${limitUser}\n`
          Profile += `💰 Uang: ${userData.uang}\n`
          Profile += `🛍 Kupon: ${userData.kupon}\n`
-         m.adsReply(Profile, picture, m.chat)
-      } else {
-         return m.reply('Profil tidak ditemukan');
+         m.adsReply(Profile, picture, m.chat);
       }
    }
 };
