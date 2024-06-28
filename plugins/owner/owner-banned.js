e6f64104440af586f42a81e87b75f6b14a7e0cf9
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-29 05:16:06 +0700

    Update owner-banned.js

--- plugins/owner/owner-banned.js
+++ plugins/owner/owner-banned.js
@@ -8,13 +8,14 @@ export default {
       User,
       command
    }) => {
-      if (!text) return m.reply(`Masukkan Nomor dan alasan nya Contoh: ${prefix+command} nomor(spasi)alasan\nContoh: ${prefix+command} 62xxxxx karena dia toxic atau menghina bot`);
-      let [number, ...reason] = text.split(" ");
-      if (!reason.length) return m.reply(`Masukan Alasannya \nContoh: ${prefix+command} nomor(spasi)alasan\nContoh: ${prefix+command} 62xxxxx karena dia toxic atau menghina bot`)
-      let num = number + '@s.whatsapp.net';
+      if (!text) return m.reply(`Masukkan Nomor dan alasan nya Contoh: ${prefix+command} nomor.alasan\nContoh: ${prefix+command} +62 878-7855-4275.karena dia toxic atau menghina bot\n\nlihat profile yang mau kamu banned dan salin nomornya terus tempel`);
+      let number = text.split(".")[0]
+      let reason = text.split(".")[1]
+      if (!reason) return m.reply(`Masukan Alasannya \nContoh: ${prefix+command} nomor(spasi)alasan\nContoh: ${prefix+command} 62xxxxx karena dia toxic atau menghina bot`)
+      let num = `${number.replace("-", "").replace("-", "").replace("+62 ", "62")}@s.whatsapp.net`
       User.addBannedUser(num, true);
-      User.bannedReason(num, reason.join(" "));
-      m.reply(`Nomor ${num} berhasil di banned\nSekarang Nomor Itu Tidak Bisa Menggunakan Bot Ini\nAlasan Di Banned:\n${reason.join(" ")} \nUntuk melihat daftar banned ketik .listbanned`);
+      User.bannedReason(num, reason);
+      m.reply(`Nomor ${num} berhasil di banned\nSekarang Nomor Itu Tidak Bisa Menggunakan Bot Ini\n\nAlasan Di Banned:\n${reason} \n\nUntuk melihat daftar banned ketik .listbanned`);
    },
    owner: true
 };
