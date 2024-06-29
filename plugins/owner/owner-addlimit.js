e5534aa834d73e9980035a23958a5af2215e67e9
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-06-29 09:00:56 +0700

    Update owner-addlimit.js

--- plugins/owner/owner-addlimit.js
+++ plugins/owner/owner-addlimit.js
@@ -8,9 +8,11 @@ export default {
       User,
       command
    }) => {
-      if (!text) return m.reply(`Masukkan nilai limitnya. Contoh: ${prefix+command} nomor limit\nContoh: ${prefix+command} 62xxxxx 25`);
-      let [number, limit] = text.split(" ");
-      let num = number + "@s.whatsapp.net";
+      if (!text) return m.reply(`Masukkan nomor dan limitnya \natau tag yang mau di tambahkan limit dan masukan limitnya \nContoh: ${prefix+command} nomor limit\nContoh: ${prefix+command} 62xxxxx 25 \nAtau\nContoh: ${prefix+command} @tag 25`);
+      let number = text.split(" ")[0]
+      let limit = text.split(" ")[1]
+      if (!limit) return m.reply(`Masukan limitnya berupa angka \nContoh: ${prefix+command} nomor(spasi)limit\nContoh: ${prefix+command} 62xxxxx 25\nAtau \nContoh: ${prefix+command} @tag 25`)
+      let num = `${number.replace("@", "").trim()}@s.whatsapp.net`      
       User.addLimitUser(num, limit);
       m.reply(`Berhasil Menambahkan ${limit} Limit Ke ${num}`);
    },
