21a70667ab94def4943900fa95fd6e8b7b8fa202
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-07-03 07:04:42 +0700

    Add files via upload

---
+++ plugins/tools/get-fetch.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+export default {
+   names: ['Tools'],
+   tags: ['fetch'],
+   command: ['fetch', 'get'],
+   start: async (m, {
+      conn,
+      text,
+      prefix,
+      command
+   }) => {
+      if (!text) return m.reply(`masukan link atau url \ncontoh: ${prefix+command} https://www.google.com`);
+      if (!text.match(/http|https/g)) return m.reply(`link salah awali dengan http atau https`);
+      m.reply(loading);
+      let res = await fetch(text);
+      if (res.headers.get('content-length') > 100 * 1024 * 1024 * 1024) {
+         throw `Content-Length: ${res.headers.get('content-length')}`
+      }
+      if (!/text|json/.test(res.headers.get('content-type'))) {
+         return conn.sendFile(m.chat, text, {
+            quoted: m
+         });
+      }
+      let txt = await res.buffer();
+      try {
+         txt = JSON.stringify(JSON.parse(txt + ''), null, 4);
+      } catch (e) {
+         txt = txt + ''
+      } finally {
+         m.reply(txt.slice(0, 65536) + '')
+      }
+   },
+   limit: 3
+};
