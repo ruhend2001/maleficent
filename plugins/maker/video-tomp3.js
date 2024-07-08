4baf95b42de0aaae6ff06dabba02ecfb67740909
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-07-08 17:39:29 +0700

    Update video-tomp3.js

--- plugins/maker/video-tomp3.js
+++ plugins/maker/video-tomp3.js
@@ -11,21 +11,17 @@ export default {
       Format
    }) => {
       if (/audio|video/.test(mime) || m.mtype === 'videoMessage' || m.mtype === 'documentMessage' || m.mtype === 'audioMessage') {
-         if (!quoted) return
-         let buffer = await quoted.download();
          m.adReply(mess.wait, setting.thumbnail, m.chat)
-         let audio = await Format.toAudio(buffer).catch(e => { 
-            return m.reply('terjadi kesalah mungkin videonya jelek atau mungkin durasi video mungkin terlalu pendek dan panjang\n' + 'status code: '+ String(e))
-            console.error(e)
-         });
+         let media = await quoted.download()
+         let audio = await Format.mp3(media);
          conn.sendFile(m.chat, audio, {
             mimetype: 'audio/mp4',
             ptt: true,
             quoted: m
          })
       } else {
-         m.reply(`reply video atau kirim video dengan pesan ${prefix+command}`)
+         return m.reply(`reply video atau kirim video dengan pesan ${prefix+command}`)
       }
    },
-   limit: true
+   limit: 2
 };
