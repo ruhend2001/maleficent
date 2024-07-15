0b863526a284d357d673802a544cc6fd64e47585
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-07-15 09:29:37 +0700

    Update open-ai.js

--- plugins/tools/open-ai.js
+++ plugins/tools/open-ai.js
@@ -10,19 +10,11 @@ export default {
       Format
    }) => {
       if (!text) return m.reply(`contoh ${prefix+command} apa kabar?`);
-      try {
-         m.react('🟡', m.chat);     
-         let chat = await Format.openAI(text);
-         m.adReply(loading, setting.thumbnail, m.chat).then(() => {      
-            m.adReply(chat, setting.thumbnail, m.chat);
-         });
-      } catch {        
-         m.react('🟢', m.chat);     
-         let { response } = await Format.Beheaded(text);
-         m.adReply(loading, setting.thumbnail, m.chat).then(() => {      
-            return m.adReply(response, setting.thumbnail, m.chat);
-         });
-      }
+      m.react('🕒', m.chat);
+      let result = await Format.openAI(text);
+      m.adReply(loading, setting.thumbnail, m.chat).then(() => {      
+         m.adReply(`${result}`, setting.thumbnail, m.chat);
+      });
    },
    limit: 3,
    register: true
