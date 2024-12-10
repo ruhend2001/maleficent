cba71f065cd2918b09131e61e8f854eb0972ec59
Ruly Henderson <76590425+ruhend2001@users.noreply.github.com>
2024-12-10 08:45:58 +0700

    Update youtube-play.js

--- plugins/download/youtube-play.js
+++ plugins/download/youtube-play.js
@@ -15,7 +15,8 @@ exports.default = {
       const vid = (await ytsearch(text)).video[0]
       const { title, description, videoId, durationH, viewH, publishedTime } = vid; 
       if (!vid) return m.reply('Tidak di temukan, coba untuk membalikkan judul dan author nya');
-      const url = 'http://youtu.be/' + videoId;
+      const url = `https://youtu.be/${videoId}`;
+      const thumb = `https://i.ytimg.com/vi/${videoId}/0.jpg`;
       let play = `🎧 〔 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 〕\n`
       play += ` ${javi} *Data Di Temukan*\n`
       play += ` ⬡ Judul: ${title}\n`
@@ -24,10 +25,23 @@ exports.default = {
       play += ` ⬡ Upload: ${publishedTime}\n`
       play += ` ⬡ Link: ${url}\n\n`
       play += ` *Loading audio sedang dikirim...*`
-      conn.sendFile(m.chat, `https://i.ytimg.com/vi/${videoId}/0.jpg`, play, m);
       const { audio } = await ytmp3(url);
+      conn.adReply(m.chat, play, thumb, m);      
       const pretty = await Format.mp3Play(audio);
-      conn.sendFile(m.chat, pretty, title, m);
+      conn.sendFile(m.chat, pretty, title, m, {
+         mimetype: 'audio/mp4',
+         fileName: title,
+         contextInfo: {
+            externalAdReply: {
+               mediaType: 2,
+               mediaUrl: url,
+               title: title,
+               body: setting.botName,
+               sourceUrl: url,
+               thumbnail: await (await fetch(thumb)).buffer()
+            }
+         }
+      })
    },
    limit: 3
 }
