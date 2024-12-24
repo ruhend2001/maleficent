3d2658dedae590b7402d80de450b02ff9187b88d
zahwa45 <zahwazein45@gmail.com>
2024-12-24 01:07:29 +0000

    *

--- plugins/group/group-kick.js
+++ plugins/group/group-kick.js
@@ -7,15 +7,10 @@ exports.default = {
       mentionUser,
       mentionByReply
    }) => {
-      if (mentionUser.length !== 0) {
+      if (mentionUser.length !== 0 || mentionByReply) {
          await conn.groupParticipantsUpdate(m.chat, [mentionUser[0]], "remove");
          m.reply(`Berhasil Menghapus ${mentionUser} Dari Grup Ini`)
-      } else if (mentionByReply) {
-         await conn.groupParticipantsUpdate(m.chat, [mentionByReply], "remove");
-         m.reply(`Berhasil Menghapus ${mentionByReply} Dari Grup Ini`)
-      } else {
-         return m.reply(`Tag Yang Mau Di Kick`)
-      }
+      };
    },
    group: true,
    admin: true
