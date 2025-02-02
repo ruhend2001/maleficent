a77a5ba8a28ae4eb0dcb64f211bb9616189e1a1b
ruhend <you@example.com>
2025-02-02 18:26:10 +0700

    *

---
+++ plugins/tools/tools-rvo.js
@@ -0,0 +1,11 @@
+exports.default = {
+   names: ['Tools'],
+   tags: ['rvo', 'readviewonce', 'lihat'],
+   command: ['rvo', 'readviewonce', 'lihat'],
+   start: async (m, {
+      conn
+   }) => {
+      await conn.viewOnce(m, m.chat)
+   },
+   limit: 2
+};
