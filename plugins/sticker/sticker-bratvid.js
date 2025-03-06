e69a9dfd46aaf763a02cd01144eee9e39591077f
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-03-06 19:26:44 +0700

    *

--- plugins/sticker/sticker-bratvid.js
+++ plugins/sticker/sticker-bratvid.js
@@ -4,21 +4,18 @@ exports.default = {
    command: ['bratvid', 'bratvideo'],
    start: async (m, {
       conn,
-      prefix,
       text,
-      command,
-      Format
+      prefix,
+      command
    }) => {
       if (!text) return m.reply(`Kirim perintah ${prefix+command} text\ncontoh: ${prefix+command} ${setting.botName}`);
-      const pack = setting.botName
-      const own = setting.footer
-      const result = await BUFFER_URL('https://brat.caliphdev.com/api/brat/animate?text=' + text);
+      const result = await BUFFER_URL(`https://fastrestapis.fasturl.cloud/maker/brat/animated?text=${text}&mode=animated`);
       conn.adReply(m.chat, loading, cover, m).then(() => {            
          conn.sendImageAsSticker(m.chat, result, m, {
-           packname: pack,
-           author: `${own}\ncreated : \n${waktu.tanggal}\n${waktu.time} ${waktu.suasana}`
+           packname: setting.botName,
+           author: `${setting.footer}\ncreated: \n${waktu.tanggal}\n${waktu.time} ${waktu.suasana}`
          })
       })
    },
-   limit: true
+   limit: 2
 }
