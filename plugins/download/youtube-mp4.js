005ca02167219f5353c1926ea41397c2158bdae4
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-03-21 07:54:40 +0700

    *

--- plugins/download/youtube-mp4.js
+++ plugins/download/youtube-mp4.js
@@ -10,17 +10,18 @@ exports.default = {
       command
    }) => {
       if (!text) return m.reply(`Masukan Link Youtubenya contoh:\n${prefix+command} https://youtu.be/MvsAesQ-4zA`);
-      const video = await savetube.download(text, '720').catch(async () => await savetube.download(text, '1080')).catch(async () => await savetube.download(text, '480')).catch(async () => await savetube.download(text, '360'));
+      m.react('💿')
+      const video = await savetube.download(text, '1080').catch(async () => await savetube.download(text, '720')).catch(async () => await savetube.download(text, '480')).catch(async () => await savetube.download(text, '360'));
       let caption = `${head("𝐘𝐎𝐔𝐓𝐔𝐁𝐄")} \n`
       caption += `⭔ *Title:* ${video.result.title}\n`     
       caption += `⭔ *Duration:* ${video.result.duration}\n\n`
       caption += `*Loading video sedang di kirim*`
       conn.adReply(m.chat, caption, video.result.thumbnail || cover, m, {
          showAds: true
-      }).then(() => {
+      }).then(() => { // via document recommended
          conn.sendFile(m.chat, video.result.download, '', m, {
             document: true,
-            fileName: `${video.result.title}-${video.result.quality || ''}~Ruhend-MD.mp4`,
+            fileName: `${video.result.title}-${video.result.quality || ''}p~Ruhend-MD.mp4`,
             mimetype: 'video/mp4'
          })
       })
--- plugins/owner/owner-sesi.js
+++ plugins/owner/owner-sesi.js
@@ -1,22 +1,19 @@
-const fs = require('fs');
-const { exec } = require('child_process');
 exports.default = {
    names: ['Owner'],
    tags: ['sesi', 'getsesi'],
-   command: ['sesi', 'getsesi'],
+   command: ['sesi', 'sessi', 'getsesi'],
    start: async (m, {
-      conn 
+      conn,
+      Format
    }) => {     
-      m.reply(`Tunggu sedang mengambil file sesi...`).then(() => {
-         exec('zip -r tmp/sessions.zip sessions/creds.json sessions/app*', () => {
-            conn.sendFile(m.chat, fs.readFileSync('./tmp/sessions.zip'), '', m, {
-               document: true,
-               fileName: 'sessions.zip',
-               mimetype: 'application/zip'
-            })
-         })     
+      m.reply(`Tunggu sedang mengambil file sessi...`);
+      const data = await Format.sessions();
+      conn.sendFile(m.chat, data, '', m, {
+         document: true,
+         fileName: 'sessions.zip',
+         mimetype: 'application/zip'
       })
    },
    owner: true,
    private: true
-}
+};
