b312380419eb391aab13961e99e1edfae8bc4467
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-05-07 17:06:17 +0700

    *

--- plugins/main/poll.js
+++ plugins/main/poll.js
@@ -5,7 +5,7 @@ exports.default = {
    start: async (m, {
       conn
    }) => {
-      const poll = ['ya', 'menu', 'tes']
+      const poll = ['ya', 'menu', '.tiktok https://vt.tiktok.com/ZSYfBvx5d/', 'tes']
       conn.sendPoll(m.chat, 'Ini adalah contoh poll', poll, m)
    }
 }
