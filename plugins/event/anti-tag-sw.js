f65ae305e9d56c3b9f257506b127d9fcffe6239a
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-05-21 13:09:16 +0700

    *

--- plugins/event/anti-tag-sw.js
+++ plugins/event/anti-tag-sw.js
@@ -2,14 +2,21 @@ module.exports = {
    start: async (m, {
       conn,
       isAdmins,
-      isOwner
+      isOwner,
+      isBotAdmins,
+      Format
    }) => {
       try {
          if (db.chats[m.chat].tagsw && !isAdmins && !isOwner && Object?.keys(statusMentions)?.length > 1) {
-            conn.reply(statusMentions.key.remoteJid, `Terdeteksi Pansos Caper Tag Status Ke Group`, statusMentions)
-            conn.sendMessage(m.chat, {
-              delete: statusMentions.key
-           }), statusMentions = {}
+            conn.reply(statusMentions.key.remoteJid, `Terdeteksi Pansos Caper Tag Status Ke Group`, statusMentions)            
+            if (isBotAdmins) {
+               conn.sendMessage(m.chat, {
+                  delete: statusMentions.key
+               }).then(() => {
+                  conn.groupParticipantsUpdate(statusMentions.key.remoteJid, [statusMentions.key.participant], 'remove');
+               })
+            }
+            await Format.sleep(3000).then(() => statusMentions = {})           
         }
       } catch {
          return false
