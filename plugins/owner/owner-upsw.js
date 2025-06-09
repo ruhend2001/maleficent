e2684af34530e4cd472bd48fcccc1d2815ea0fe7
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-06-09 14:39:24 +0700

    *

--- plugins/owner/owner-upsw.js
+++ plugins/owner/owner-upsw.js
@@ -11,19 +11,20 @@ exports.default = {
       const contacts = Object.keys(db.contacts);
       if (Object.keys(db.contacts) < 1) throw 'Tidak Ada Kontak Yang Temukan, Tidak Bisa Mengirim Status!'                        
       const statusJidList = Object.keys(db.contacts)      
-      if (/image|video|audio/.test(mime)) {         
-         m.reply(loading)  
+      if (/image|video|audio/.test(mime)) {                  
          const media = await quoted.download();                
          if (/audio/.test(mime)) {
-            return conn.sendStatusAudio(media, statusJidList)
+            m.reply(loading)  
+            return await conn.sendStatusAudio(media, statusJidList), m.reply('Done')
          } else {
+            m.reply(loading)
             const media_2 = /image/.test(mime) ? 'image' : 'video';           
-            return conn.sendStatusImageOrVideo(text, quoted, media, media_2, statusJidList), m.reply('Done')
+            return await conn.sendStatusImageOrVideo(text, quoted, media, media_2, statusJidList), m.reply('Done')
          }
-      } else {   
-         m.reply(loading)  
-         if (!text) throw 'textnya mana?'     
-         return conn.sendStatusText(text, statusJidList), m.reply('Done')
+      } else {  
+         if (!text) throw 'textnya mana?' 
+         m.reply(loading)      
+         return await conn.sendStatusText(text, statusJidList), m.reply('Done')
       }               
    },
    owner: true
