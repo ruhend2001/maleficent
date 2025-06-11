a6136828c50ef8f5c3637cb620fa8ce44b4c0aed
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-06-11 14:54:48 +0700

    ^

--- plugins/owner/owner-upsw.js
+++ plugins/owner/owner-upsw.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 exports.default = {
    names: ['Owner'],
    tags: ['upsw', 'uploadsw'],
@@ -8,24 +9,43 @@ exports.default = {
       mime,
       quoted
    }) => {
-      const contacts = Object.keys(db.contacts);
-      if (Object.keys(db.contacts) < 1) throw 'Tidak Ada Kontak Yang Temukan, Tidak Bisa Mengirim Status!'                        
-      const statusJidList = Object.keys(db.contacts)      
-      if (/image|video|audio/.test(mime)) {                  
-         const media = await quoted.download();                
-         if (/audio/.test(mime)) {
-            m.reply(loading)  
-            return await conn.sendStatusAudio(media, statusJidList), m.reply('Done')
+      var contacts
+      try {
+         if (db.contacts.length === 0) {
+            contacts = {}
          } else {
-            m.reply(loading)
-            const media_2 = /image/.test(mime) ? 'image' : 'video';           
-            return await conn.sendStatusImageOrVideo(text, quoted, media, media_2, statusJidList), m.reply('Done')
+            contacts = db.contacts
          }
-      } else {  
-         if (!text) throw 'textnya mana?' 
-         m.reply(loading)      
-         return await conn.sendStatusText(text, statusJidList), m.reply('Done')
-      }               
+      } catch (e) {
+         console.log('Error accessing db.contacts:', e);
+      };
+      if (Object.keys(contacts).length === 0) {
+         try {
+            const data = fs.readFileSync('./lib/contacts.json', 'utf8');
+            contacts = JSON.parse(data);
+         } catch (e) {
+            console.error('Error reading contacts.json:', e);
+            throw 'Tidak ada kontak yang ditemukan, tidak bisa mengirim status!';
+         }
+      };
+      const contactKeys = Object.keys(contacts);
+      if (contactKeys.length === 0) throw 'Tidak ada kontak yang ditemukan, tidak bisa mengirim status!';
+      const statusJidList = contactKeys;      
+      if (/image|video|audio/.test(mime)) {
+         const media = await quoted.download();
+         if (/audio/.test(mime)) {
+            m.reply(loading);
+            return await conn.sendStatusAudio(media, statusJidList), m.reply('Done');
+         } else {
+            m.reply(loading);
+            const mediaType = /image/.test(mime) ? 'image' : 'video';
+            return await conn.sendStatusImageOrVideo(text, quoted, media, mediaType, statusJidList), m.reply('Done');
+         }
+      } else {
+         if (!text) throw 'Textnya mana?';
+         m.reply(loading);
+         return await conn.sendStatusText(text, statusJidList), m.reply('Done');
+      }
    },
    owner: true
-}
+};
