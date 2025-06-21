081fd24f8f9c21d8937330d2f1b6e9aadb03f48f
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-06-21 11:36:34 +0700

    *

--- plugins/tools/tools-img-circle.js
+++ plugins/tools/tools-img-circle.js
@@ -12,11 +12,15 @@ exports.default = {
       Format     
    }) => {
       if (/image/.test(mime) || m.mtype === 'imageMessage') {
-         m.react('🕒');
-         const content = await quoted.download();
-         conn.adReply(m.chat, loading, cover, m);
-         const image = text ? await Format.circle_photo(content, parseInt(text)) : await Format.circle_photo(content);
-         conn.sendFile(m.chat, image, '', m); 
+         try {
+            m.react('🕒');
+            const content = await quoted.download();
+            conn.adReply(m.chat, loading, cover, m);
+            const image = text ? await Format.circle_photo(content, parseInt(text)) : await Format.circle_photo(content);
+            conn.sendFile(m.chat, image, '', m);
+         } catch {
+            throw 'Error when converting !'
+         } 
       } else {
         return m.reply(`Balas atau kirim gambar dengan caption ${prefix+command} yang mau di jadikan ke bulat\noptional kamu dapat menyesuaikan ukuran gambar contoh:\n${prefix+command} 300`)
       }
