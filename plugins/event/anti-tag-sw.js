6c59fc9aa163664d34406baa0f93192f106ca74e
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-07-07 20:47:02 +0700

    *

--- plugins/event/anti-tag-sw.js
+++ plugins/event/anti-tag-sw.js
@@ -1,26 +1,27 @@
 module.exports = {
    start: async (m, {
       conn,
-      isAdmins,
-      isOwner,
-      isBotAdmins,
       Format
    }) => {
       try {
-         if (!isAdmins && !isOwner && Object?.keys(statusMentions)?.length > 1 && db.chats[statusMentions?.key?.remoteJid]?.tagsw) {
+         if (Object?.keys(statusMentions)?.length > 1 && db.chats[statusMentions?.key?.remoteJid]?.tagsw) {
+            const participants = (await conn.groupMetadata(statusMentions.key.remoteJid)).participants;
+            const groupAdmins = participants?.filter(v => v.admin !== null).map(v => v.id);
+            const isAdmins = groupAdmins?.includes(statusMentions.key.participant);
+            const isBotAdmins = groupAdmins?.includes(conn.decodeJid(conn.user.id));
+            const isOwner = [...setting.ownerNumber, conn.decodeJid(conn.user.id).split('@')[0]].map(num => `${num}@s.whatsapp.net`).includes(statusMentions.key.participant);
+            if (isOwner) return console.log('Owner sending status mentions');
+            if (isAdmins) return console.log('Admin sending status mentions');
             conn.reply(statusMentions.key.remoteJid, `*Terdeteksi Pansos Caper Tag Status Ke Group Atau Ngemis Penonton*\n*Silahkan Klik Laporkan dan Blokir Orang Ini*\n*@${statusMentions.key.participant.split("@")[0]}*\n*Agar Status Gak Guna atau Sampahnya Dia Tidak Muncul Di Menu Status Pembaruan Kalian*`, statusMentions, { contextInfo: { mentionedJid: [statusMentions.key.participant] }});           
             if (isBotAdmins) {
                conn.sendMessage(m.chat, {
                   delete: statusMentions.key
-               }).then(() => {
-                  conn.groupParticipantsUpdate(statusMentions.key.remoteJid, [statusMentions.key.participant], 'remove');
-               })
-            }
-            conn.updateBlockStatus(statusMentions.key.participant, 'block');
-            await Format.sleep(1000).then(() => statusMentions = {})           
+               }), conn.groupParticipantsUpdate(statusMentions.key.remoteJid, [statusMentions.key.participant], 'remove');               
+            };
+            conn.updateBlockStatus(statusMentions.key.participant, 'block'), statusMentions = {}
         }
       } catch (e) {
          return console.error(e)
-      }     
+      }
    }
 }
--- plugins/event/tes-decline.js
+++ plugins/event/tes-decline.js
@@ -1,6 +1,6 @@
 module.exports = {
    start: async (m) => {
-      if (m.sender === '6283866043444@s.whatsapp.net') {
+      if (m.sender === '6288213962138@s.whatsapp.net' || m.sender === '6283866043444@s.whatsapp.net') {
          return m.delete(m.key)
       }
    }   
