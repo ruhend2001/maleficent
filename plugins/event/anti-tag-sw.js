815c985cc6e18184c8e8018f9da5eba4bbffb425
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-07-09 17:19:16 +0700

    *

--- plugins/event/anti-tag-sw.js
+++ plugins/event/anti-tag-sw.js
@@ -1,7 +1,6 @@
 module.exports = {
    start: async (m, {
-      conn,
-      Format
+      conn
    }) => {
       try {
          if (Object?.keys(statusMentions)?.length > 1 && db.chats[statusMentions?.key?.remoteJid]?.tagsw && !db.chats[statusMentions?.key?.remoteJid].mute) {
@@ -11,14 +10,13 @@ module.exports = {
             const isBotAdmins = groupAdmins?.includes(conn.decodeJid(conn.user.id));
             const isOwner = [...setting.ownerNumber, conn.decodeJid(conn.user.id).split('@')[0]].map(num => `${num}@s.whatsapp.net`).includes(statusMentions.key.participant);
             if (isOwner) return console.log('Owner sending status mentions');
-            if (isAdmins) return console.log('Admin sending status mentions');
-            conn.reply(statusMentions.key.remoteJid, `*Terdeteksi Pansos Caper Tag Status Ke Group Atau Ngemis Penonton*\n*Silahkan Klik Laporkan dan Blokir Orang Ini*\n*@${statusMentions.key.participant.split("@")[0]}*\n*Agar Status Gak Guna atau Sampahnya Dia Tidak Muncul Di Menu Status Pembaruan Kalian*`, statusMentions, { contextInfo: { mentionedJid: [statusMentions.key.participant] }});           
-            await Format.sleep(3000);
+            if (isAdmins) return console.log('Admin sending status mentions');                        
+            conn.reply(statusMentions.key.remoteJid, `*Terdeteksi Pansos Caper Tag Status Ke Group Atau Ngemis Penonton*\n*Silahkan Klik Laporkan dan Blokir Orang Ini*\n*@${statusMentions.key.participant.split("@")[0]}*\n*Agar Status Gak Guna atau Sampahnya Dia Tidak Muncul Di Menu Status Pembaruan Kalian*`, statusMentions, { contextInfo: { mentionedJid: [statusMentions.key.participant] }});
             if (isBotAdmins) {
                conn.sendMessage(m.chat, {
                   delete: statusMentions.key
                }), conn.groupParticipantsUpdate(statusMentions.key.remoteJid, [statusMentions.key.participant], 'remove');               
-            };
+            }         
             conn.updateBlockStatus(statusMentions.key.participant, 'block'), statusMentions = {}
         }
       } catch (e) {
