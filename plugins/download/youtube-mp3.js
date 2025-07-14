0dc1f2a7753d76592db7a4e25f61b94792b58d7b
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-07-14 20:19:48 +0700

    *

--- plugins/download/youtube-mp3.js
+++ plugins/download/youtube-mp3.js
@@ -1,4 +1,5 @@
 const savetube = require('../../lib/src/scraper/savetube.js');
+const ocean = require('../../lib/src/scraper/ocean.js');
 exports.default = {
    names: ['Downloader'],
    tags: ['ytmp3'],
@@ -12,15 +13,15 @@ exports.default = {
    }) => {
       if (!text) return m.reply(`Masukan kontolnya! \nContoh: ${prefix+command} https://youtu.be/MvsAesQ-4zA`);
       m.react("🕗")
-      const audio = await savetube.download(text, '144');
-      conn.adReply(m.chat, loading, audio.result.thumbnail || cover, m);
-      const media = await Format.mp3(await toBuffer(audio.result.download));   
+      const audio = await savetube.download(text, '144').catch(async () => await ocean(text, 'mp3'))
+      conn.adReply(m.chat, loading, audio?.result?.thumbnail || audio?.thumbnail || cover, m);
+      const media = await Format.mp3(await toBuffer(audio?.result?.download || audio?.link));   
       conn.sendFile(m.chat, media, '', m, {
          document: true,
-         fileName: `${audio.result.title}~Ruhend-MD.mp3`,
+         fileName: `${audio?.result?.title || audio?.title}~Ruhend-MD.mp3`,
          mimetype: 'audio/mpeg'
       })
    },
-   limit: 3,
+   limit: 2,
    premium: false
 }
--- plugins/download/youtube-play.js
+++ plugins/download/youtube-play.js
@@ -1,5 +1,6 @@
 const search = require("yt-search");
 const savetube = require('../../lib/src/scraper/savetube.js');
+const ocean = require('../../lib/src/scraper/ocean.js');
 exports.default = {
    names: ['Downloader'],
    tags: ['play', 'song', 'lagu'],
@@ -12,23 +13,42 @@ exports.default = {
       Format
    }) => {
       if (!text) return m.reply(`Masukan Lagu Yang Ingin Di Cari\ncontoh ${prefix+command} papinka sana sini aku rindu atau .play linknya https://youtu.be/A5Jj6Ib91zA`);
-      let data = await search(text), res = data.all, url = data.videos[0], thumb = `https://i.ytimg.com/vi/${url.videoId}/0.jpg`, result = '';
-      result += `🎧 〔 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 〕\n`      
-      result += `*⭔ Title:* ${url.title}\n`
-      result += `*⭔ Durasi:* ${url.timestamp}\n`
-      result += `*⭔ Views:* ${url.views.toLocaleString()}\n`
-      result += `*⭔ Name Channel:* ${url.author.name}\n`
-      result += `*⭔ Channel:* ${url.author.url}\n`
-      result += `*⭔ URL Video:* ${url.url}\n\n`
-      result += ` *Loading audio sedang dikirim...*`      
-      conn.adReply(m.chat, result, thumb, m);
-      const audio = await savetube.download(url.url, '144');
-      const pretty = await Format.mp3Play(await toBuffer(audio.result.download));
+      let data, res, url, thumb, audio, link, result = '', pretty   
+      try {
+         data = await search(text); res = data.all; url = data.videos[0]; link = url.url; thumb = `https://i.ytimg.com/vi/${url.videoId}/0.jpg`;
+         result += `🎧 〔 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 〕\n`      
+         result += `*⭔ Title:* ${url.title}\n`
+         result += `*⭔ Durasi:* ${url.timestamp}\n`
+         result += `*⭔ Views:* ${url.views.toLocaleString()}\n`
+         result += `*⭔ Name Channel:* ${url.author.name}\n`
+         result += `*⭔ Channel:* ${url.author.url}\n`
+         result += `*⭔ URL Video:* ${url.url}\n\n`
+         result += ` *Loading audio sedang dikirim...*`      
+         conn.adReply(m.chat, result, thumb, m);
+         audio = await savetube.download(link, '144');
+         if (!audio) m.react('❎'); else m.react('✅');
+         pretty = await Format.mp3Play(await toBuffer(audio.result.download));      
+      } catch {
+         /** if vevo or original sometimes it doesn't supported with savetube **/
+         result = ''; data = await search(text); res = data.all; url = data.videos[0]; link = url.url; thumb = `https://i.ytimg.com/vi/${url.videoId}/0.jpg`;
+         result += `🎧 〔 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 〕\n`      
+         result += `*⭔ Title:* ${url.title}\n`
+         result += `*⭔ Durasi:* ${url.timestamp}\n`
+         result += `*⭔ Views:* ${url.views.toLocaleString()}\n`
+         result += `*⭔ Name Channel:* ${url.author.name}\n`
+         result += `*⭔ Channel:* ${url.author.url}\n`
+         result += `*⭔ URL Video:* ${url.url}\n\n`
+         result += ` *Loading audio sedang dikirim...*`      
+         conn.adReply(m.chat, result, thumb, m);
+         audio = await ocean(link, 'mp3');
+         if (!audio) m.react('❎'); else m.react('✅');
+         pretty = await Format.mp3Play(await toBuffer(audio.link));     
+      };
       conn.sendFile(m.chat, pretty, url.title, m, {
          contextInfo: {
             externalAdReply: {
                mediaType: 2,
-               mediaUrl: url.url,
+               mediaUrl: link,
                title: url.title,
                body: setting.botName,
                sourceUrl: url.title,
