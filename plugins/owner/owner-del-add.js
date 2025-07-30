1f8a113279b14094e9f31b61f308668ed46f9d03
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-07-30 16:10:52 +0700

    *

--- plugins/owner/owner-del-add.js
+++ plugins/owner/owner-del-add.js
@@ -8,18 +8,19 @@ exports.default = {
       command
    }) => {
       if (/addowner|addown/.test(command)) {
-         if (!text) return m.reply(`Masukkan Nomornya contoh\n${prefix+command} 62xxxx`);
-         let num = `${text.replace("@", "").replace("-", "").replace("-", "").replace("+62 ", "62").trim()}`;
+         if (!text) return m.reply(`Masukkan Nomornya contoh\n${prefix+command} 62xxxx atau tag nomornya`);
+         const num = `${text.replace(/[@+\s-]/g, '').trim()}`;
          setting.ownerNumber.push(num);
          save_setting()
          m.reply(`Sukses Menambahkan ${num} sebagai owner`);
       } else if (/delowner|delown/.test(command)) {
          if (!text) return m.reply(`Masukkan Nomor yang ingin dihapus dari daftar owner.`);
-         let index = setting.ownerNumber.indexOf(text);
+         const num = `${text.replace(/[@+\s-]/g, '').trim()}`;
+         const index = setting.ownerNumber.indexOf(num);
          if (index !== -1) {
             setting.ownerNumber.splice(index, 1);
             save_setting()
-            m.reply(`Nomor ${text} berhasil dihapus dari daftar owner.`);
+            m.reply(`Nomor ${text.replace('@', '')} berhasil dihapus dari daftar owner.`);
          } else {
             return m.reply(`Nomor ${text} tidak ditemukan dalam daftar owner.`);
          }
