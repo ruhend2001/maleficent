f526e6ff9e9a5cfe03c105ca1a283954988cdb50
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-08-02 10:20:35 +0700

    *

--- main.js
+++ main.js
@@ -8,15 +8,9 @@ process.on('uncaughtException', (e) => {
    } else console.error(e);
 });
 const pino = require('pino');
-const {
-   makeInMemoryStore,
-   useMultiFileAuthState,
-   DisconnectReason
-} = require('@baileys-md/baileys');
 const { Format, Connect, Signal } = require('utils-mf'); 
 Object.assign(global, { default_db: { users: {}, chats: {}, settings: {}, stores: {}, menfess: {}, contacts: {} }, setting: require('./config.json'), mess: require('./lib/message.js'), Connect: Connect, Format: Format, moment: require("moment-timezone") });
 moment.tz.setDefault("Asia/Jakarta").locale("id");
-const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' })});      
 require('./lib/settings.js');
 require('utils-mf/index.js');
 require('./lib/system.js');
@@ -24,7 +18,9 @@ require('./lib/src/cloud/mongo-db.js');
 require('./lib/src/cloud/github-db.js');
 const { caller } = require('./lib/system.js');
 const startWhatsApp = async () => {
+   const { makeInMemoryStore, useMultiFileAuthState, DisconnectReason } = await import('@baileys-md/baileys');
    const { state, saveCreds } = await useMultiFileAuthState('./sessions');
+   const store = await makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' })});    
    const conn = await Signal(state, store); 
    store.bind(conn.ev); caller(conn);
    conn.ev.on('creds.update', () => saveCreds());
