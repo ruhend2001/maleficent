a6bb06320ccb29736f40fc2c3741d7c23d395f40
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-08-10 19:23:01 +0700

    *

--- plugins/event/anti-tag-sw.js
+++ plugins/event/anti-tag-sw.js
@@ -9,9 +9,9 @@ module.exports = {
             const groupAdmins = participants?.filter(v => v.admin !== null).map(v => v.id);
             const isAdmins = groupAdmins?.includes(statusMentions.key.participant);
             const isBotAdmins = groupAdmins?.includes(conn.decodeJid(conn.user.id));
-            const isOwner = [...setting.ownerNumber, conn.decodeJid(conn.user.id).split('@')[0]].map(num => `${num}@s.whatsapp.net`).includes(statusMentions.key.participant);
-            if (isOwner) return console.log('Owner sending status mentions');
-            if (isAdmins) return console.log('Admin sending status mentions');                        
+            const isOwner = statusMentions.key.participant.endsWith('@lid') ? [...setting.ownerNumber, conn.decodeJid(conn.user.id).split('@')[0]].map(num => `${num}@lid`).includes(statusMentions.key.participant) : [...setting.ownerNumber, conn.decodeJid(conn.user.id).split('@')[0]].map(num => `${num}@s.whatsapp.net`).includes(statusMentions.key.participant);
+            if (isOwner) return console.log('Owner sending status mentions'), statusMentions = {};
+            if (isAdmins) return console.log('Admin sending status mentions'), statusMentions = {};                  
             conn.reply(statusMentions.key.remoteJid, `*Terdeteksi Pansos Caper Tag Status Ke Group Atau Ngemis Penonton*\n*Silahkan Klik Laporkan dan Blokir Orang Ini*\n*@${statusMentions.key.participant.split("@")[0]}*\n*Agar Status Gak Guna atau Status Sampah Dia Tidak Muncul Di Menu Status Pembaruan Kalian*`, statusMentions, { contextInfo: { mentionedJid: [statusMentions.key.participant] }});
             if (isBotAdmins) {
                await conn.sendMessage(statusMentions.key.remoteJid, {
@@ -19,7 +19,7 @@ module.exports = {
                });
                await conn.groupParticipantsUpdate(statusMentions.key.remoteJid, [statusMentions.key.participant], 'remove');
             };
-            conn.updateBlockStatus(statusMentions.key.participant, 'block'), await Format.sleep(1000), statusMentions = {}
+            return await conn.updateBlockStatus(statusMentions.key.participant, 'block'), statusMentions = {};
         }
       } catch (e) {
          return console.error(e)
