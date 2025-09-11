749f9198270d59747ff622a2c94628df0bbee5b5
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-09-11 07:04:30 +0700

    *

--- plugins/games/games-answer/tictactoe.js
+++ plugins/games/games-answer/tictactoe.js
@@ -1,26 +1,23 @@
-let rewards = {
+const rewards = {
    limit: 15,
    uang: 30
-}
+};
 module.exports = {
    start: async (m, {
       conn,
       budy
    }) => {
-      let room = Object.values(tictactoe).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING')      
-      let parseMention = (text = '') => {
-         return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
-      }
-       if (room) {
+      let room = Object.values(tictactoe).find(room => room.id && room.game && room.state && room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender) && room.state == 'PLAYING');
+      if (room) {
          let ok
          let isWin = !1
          let isTie = !1
          let isSurrender = !1
          if (!/^([1-9]|(me)?nyerah|surr?ender|off|skip)$/i.test(m.text)) return
          isSurrender = !/^[1-9]$/.test(m.text)
-         if (m.sender !== room.game.currentTurn) { // nek wayahku
+         if (m.sender !== room.game.currentTurn) {
             if (!isSurrender) return !0
-         }
+         };
          if (!isSurrender && 1 > (ok = room.game.turn(m.sender === room.game.playerO, parseInt(m.text) - 1))) {
             m.reply({
                '-3': 'Game telah berakhir',
@@ -29,38 +26,26 @@ module.exports = {
                0: 'Posisi Invalid / Salah',
             } [ok])
             return !0
-         }
+         };
          if (m.sender === room.game.winner) isWin = true
          else if (room.game.board === 511) isTie = true
-         let arr = room.game.render().map(v => {
-            return {
-               X: '❌',
-               O: '⭕',
-               1: '1️⃣',
-               2: '2️⃣',
-               3: '3️⃣',
-               4: '4️⃣',
-               5: '5️⃣',
-               6: '6️⃣',
-               7: '7️⃣',
-               8: '8️⃣',
-               9: '9️⃣',
-            } [v]
-         })
+         const arr = room.game.render().map(v => {
+            return { X: '❌', O: '⭕', 1: '1️⃣', 2: '2️⃣', 3: '3️⃣', 4: '4️⃣', 5: '5️⃣', 6: '6️⃣', 7: '7️⃣', 8: '8️⃣', 9: '9️⃣' } [v]
+         });
          if (isSurrender) {
             room.game._currentTurn = m.sender === room.game.playerX
             isWin = true
-         }
-         let winner = isSurrender ? room.game.currentTurn : room.game.winner
-         let str = `Room ID: ${room.id}\n${arr.slice(0, 3).join('')}\n${arr.slice(3, 6).join('')}\n${arr.slice(6).join('')}\n${isWin ? `\n\n@${winner.split('@')[0]} Menang!\nGame berakhir\n@${winner.split('@')[0]} Mendapat Hadiah :\n+${rewards.limit} limit 🎟\n+${rewards.uang} uang 💰\n` : isTie ? `Game berakhir gak ada yang menang / seri` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}\n❌: @${room.game.playerX.split('@')[0]}\n⭕: @${room.game.playerO.split('@')[0]}\nKetik *nyerah* untuk menyerah dan mengakui kekalahan`
+         };
+         const winner = isSurrender ? room.game.currentTurn : room.game.winner
+         const str = `Room ID: ${room.id}\n${arr.slice(0, 3).join('')}\n${arr.slice(3, 6).join('')}\n${arr.slice(6).join('')}\n${isWin ? `\n\n@${winner.split('@')[0]} Menang!\nGame berakhir\n@${winner.split('@')[0]} Mendapat Hadiah :\n+${rewards.limit} limit 🎟\n+${rewards.uang} uang 💰\n` : isTie ? `Game berakhir gak ada yang menang / seri` : `Giliran ${['❌', '⭕'][1 * room.game._currentTurn]} (@${room.game.currentTurn.split('@')[0]})`}\n❌: @${room.game.playerX.split('@')[0]}\n⭕: @${room.game.playerO.split('@')[0]}\nKetik *nyerah* untuk menyerah dan mengakui kekalahan`
          if ((room.game._currentTurn ^ isSurrender ? room.x : room.o) !== m.chat)
             room[room.game._currentTurn ^ isSurrender ? 'x' : 'o'] = m.chat
          if (room.x !== room.o) await conn.sendText(room.x, str, m, {
-            mentions: parseMention(str)
-         })
+            mentions: m.isLid ? conn.parseMentionLid(str) : conn.parseMention(str)
+         });
          await conn.sendText(room.o, str, m, {
-            mentions: parseMention(str)
-         })
+            mentions: m.isLid ? conn.parseMentionLid(str) : conn.parseMention(str)
+         });
          if (isTie || isWin) {
             if (isWin) {
                db.users[winner].limit += rewards.limit
@@ -70,4 +55,4 @@ module.exports = {
          }
       }
    }
-};
+}
--- plugins/games/tictactoe.js
+++ plugins/games/tictactoe.js
@@ -1,8 +1,4 @@
 const TicTacToe = require('../../lib/tictactoe.js');
-let rewards = {
-   limit: 15,
-   uang: 30
-}
 exports.default = {
    names: ['Games'],
    tags: ['tictactoe', 'ttt'],
@@ -13,39 +9,24 @@ exports.default = {
       prefix,
       command
    }) => {
-      if (command == 'tictactoe' || command == 'ttt') {         
+      if (command == 'tictactoe' || command == 'ttt') {
          if (Object.values(tictactoe).find(room => room.id.startsWith('tictactoe') && [room.game.playerX, room.game.playerO].includes(m.sender))) return m.reply('Kamu masih didalam game')
-         let room = Object.values(tictactoe).find(room => room.state === 'WAITING' && (text ? room.name === text : true))
-         let parseMention = (text = '') => {
-            return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
-         }
+         let room = Object.values(tictactoe).find(room => room.state === 'WAITING' && (text ? room.name === text : true));
          if (room) {
             await m.reply('Partner ditemukan!')
             room.o = m.chat
             room.game.playerO = m.sender
             room.state = 'PLAYING'
-            let arr = room.game.render().map(v => {
-               return {
-                  X: '❌',
-                  O: '⭕',
-                  1: '1️⃣',
-                  2: '2️⃣',
-                  3: '3️⃣',
-                  4: '4️⃣',
-                  5: '5️⃣',
-                  6: '6️⃣',
-                  7: '7️⃣',
-                  8: '8️⃣',
-                  9: '9️⃣',
-               } [v]
-            })
-            let str = `Room ID: ${room.id}\n\n${arr.slice(0, 3).join('')}\n${arr.slice(3, 6).join('')}\n${arr.slice(6).join('')}\n\nMenunggu @${room.game.currentTurn.split('@')[0]}\nKetik *nyerah* untuk menyerah dan mengakui kekalahan`
+            const arr = room.game.render().map(v => {
+               return { X: '❌', O: '⭕', 1: '1️⃣', 2: '2️⃣', 3: '3️⃣', 4: '4️⃣', 5: '5️⃣', 6: '6️⃣', 7: '7️⃣', 8: '8️⃣', 9: '9️⃣' } [v]
+            });
+            const str = `Room ID: ${room.id}\n\n${arr.slice(0, 3).join('')}\n${arr.slice(3, 6).join('')}\n${arr.slice(6).join('')}\n\nMenunggu @${room.game.currentTurn.split('@')[0]}\nKetik *nyerah* untuk menyerah dan mengakui kekalahan`
             if (room.x !== room.o) await conn.sendText(room.x, str, m, {
-               mentions: parseMention(str)
-            })
+               mentions: m.isLid ? conn.parseMentionLid(str) : conn.parseMention(str)
+            });
             await conn.sendText(room.o, str, m, {
-               mentions: parseMention(str)
-            })
+               mentions: m.isLid ? conn.parseMentionLid(str) : conn.parseMention(str)
+            });
          } else {
             room = {
                id: 'tictactoe-' + (+new Date),
@@ -59,7 +40,7 @@ exports.default = {
             await m.reply(`Menunggu partner atau kamu bisa ajak member lain dengan mengetik ${prefix+command}` + (text ? ` mengetik command dibawah ini ${prefix}${command} ${text}` : ''))
          }
       } else if (command == 'delttt') {
-         if (tictactoe) {            
+         if (tictactoe) {
             tictactoe = {}
             conn.reply(m.chat, `Berhasil delete session TicTacToe`, m);
          }
