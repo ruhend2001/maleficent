4c1dff1626e261e04ee8df2e5918e6cb6f773c17
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-09-13 13:47:06 +0700

    *

--- plugins/owner/owner-addlimit.js
+++ plugins/owner/owner-addlimit.js
@@ -1,21 +1,27 @@
 exports.default = {
    names: ['Owner'],
-   tags: ['addlimit'],
-   command: ['addlimit', 'tambahlimit'],
+   tags: ['addlimit', 'tambahlimit', 'addlimitowner', 'cheatlimit'],
+   command: ['addlimit', 'tambahlimit', 'addlimitowner', 'cheatlimit'],
    start: async (m, {
       conn,
       text,
       prefix,
       command
    }) => {
-      if (!text) return m.reply(`Masukkan/ tag nomor dan limitnya \natau tag yang mau di tambahkan limit dan masukan limitnya \nContoh: ${prefix + command} nomor limit\nContoh: ${prefix + command} 62xxxxx 25 \nAtau\nContoh: ${prefix + command} @tag 25`);
-      const parts = text.split(" "), limit = parts.pop(), number = parts.join(" "); 
-      if (!limit || isNaN(limit)) return m.reply(`Masukan limitnya berupa angka \nContoh: ${prefix + command} nomor(spasi)limit\nContoh: ${prefix + command} 62xxxxx 25\nAtau \nContoh: ${prefix + command} @tag 25`);
-      const Number = conn.decodeNum(number)
-      const num = m.isLid ? Number + '@lid' : Number + '@s.whatsapp.net'
-      if (!db.users[num]) return m.reply(`Pengguna dengan nomor ${num} tidak ditemukan dalam database. Pastikan nomor sudah terdaftar.`);
-      db.users[num].limit += parseInt(limit);
-      m.reply(`Berhasil Menambahkan ${limit} Limit Ke ${num.split('@')[0]}`);
+      if (command === 'addlimit' || command === 'tambahlimit') {
+         if (!text) return m.reply(`Masukkan/ tag nomor dan limitnya \natau tag yang mau di tambahkan limit dan masukan limitnya \nContoh: ${prefix + command} nomor limit\nContoh: ${prefix + command} 62xxxxx 25 \nAtau\nContoh: ${prefix + command} @tag 25`);
+         const parts = text.split(" "), limit = parts.pop(), number = parts.join(" "); 
+         if (!limit || isNaN(limit)) return m.reply(`Masukan limitnya berupa angka \nContoh: ${prefix + command} nomor(spasi)limit\nContoh: ${prefix + command} 62xxxxx 25\nAtau \nContoh: ${prefix + command} @tag 25`);
+         const Number = conn.decodeNum(number)
+         const num = m.isLid ? Number + '@lid' : Number + '@s.whatsapp.net'
+         if (!db.users[num]) return m.reply(`Pengguna dengan nomor ${num} tidak ditemukan dalam database. Pastikan nomor sudah terdaftar.\natau coba untuk tag contoh \n.addlimit @tag 100`);
+         db.users[num].limit += parseInt(limit);
+         m.reply(`Berhasil Menambahkan ${limit} Limit Ke ${num.split('@')[0]}`);
+      } else if (command === 'addlimitowner' || command ===  'cheatlimit') {
+         if (!text) return m.reply(`Masukkan limitnya contoh: \n${prefix+command} 100`);
+         db.users[m.sender].limit += parseInt(text);
+         m.reply(`Cheat Limit ${text} Berhasil`);
+      }
    },
    owner: true
 };
