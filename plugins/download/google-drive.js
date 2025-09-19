b0d32e405ebab98f59866601aacabeb863d5b7dc
Simple Bot WhatsApp <matsunagahisahde@gmail.com>
2025-09-19 12:31:25 +0700

    *

--- plugins/download/google-drive.js
+++ plugins/download/google-drive.js
@@ -6,16 +6,16 @@ exports.default = {
       conn,
       text,
       prefix,
-      command,
-      Format
+      command
    }) => {
       if (!text) return m.reply(`Masukan Link Google Drove nya contoh ${prefix+command} https://drive.google.com/file/d/1BKaXs8uIt4_C_dEKUje-nn-XYYNOO07y/view?usp=drivesdk`)
-      let res = await Format.gdrive(text);
-      if (!res) throw res;
-      let drive = ` ${star} 𝐆𝐎𝐆𝐆𝐋𝐄 𝐃𝐑𝐈𝐕𝐄\n`
-      drive += ` ${java} Name: ${res.fileName}\n`
-      drive += ` ${java} Type: ${res.mimetype}\n`
-      drive += ` ${java} Size: ${res.fileSize}\n`
+      m.react('🕒')
+      const res = await Scraper.gdrive(text);
+      if (!res) throw res      
+      const drive = ` ${star} 𝐆𝐎𝐆𝐆𝐋𝐄 𝐃𝐑𝐈𝐕𝐄\n` +
+      ` ${java} Name: ${res.fileName}\n` +
+      ` ${java} Type: ${res.mimetype}\n` +
+      ` ${java} Size: ${res.fileSize}`
       conn.adReply(m.chat, drive, cover, m).then(() => {
          conn.sendFile(m.chat, res.downloadUrl, '', m, {
             document: true,
