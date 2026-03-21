caf896d83e39027bcc6cb1ad088e54d941a17799
ruhend2001 <matsunagahisahde@gmail.com>
2026-03-21 14:20:15 +0700

    *

--- plugins/event/assa-lam.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   start: async (m, {
-      budy
-   }) => {
-      if (budy.toLowerCase().includes('assalamualaikum') || budy.toLowerCase().includes('assalamu\'alaikum')) {
-         return m.reply('Waalaikumsalam')
-      }
-   }
-}
